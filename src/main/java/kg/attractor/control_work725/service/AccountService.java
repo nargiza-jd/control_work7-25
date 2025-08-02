448c3b399c7396ffa8636fe0f55686cb77c9d832
nargiza-jd <supabaevanargiza@gmail.com>
2025-08-02 13:05:09 +0600

    Updated AccountService and TransactionService classes by adding new methods

--- src/main/java/kg/attractor/control_work725/service/AccountService.java
+++ src/main/java/kg/attractor/control_work725/service/AccountService.java
@@ -1,7 +1,13 @@
 package kg.attractor.control_work725.service;
 
 import kg.attractor.control_work725.dto.AccountCreateDto;
+import kg.attractor.control_work725.model.Account;
+
+import java.util.List;
 
 public interface AccountService {
     void createAccount(AccountCreateDto accountDto);
+    double getAccountBalance();
+    void topUpAccount(Double amount);
+    List<Account> getAccounts();
 }
--- src/main/java/kg/attractor/control_work725/service/TransactionService.java
+++ src/main/java/kg/attractor/control_work725/service/TransactionService.java
@@ -1,7 +1,16 @@
 package kg.attractor.control_work725.service;
 
 import kg.attractor.control_work725.dto.TransactionCreateDto;
+import kg.attractor.control_work725.model.Transaction;
+
+import java.util.List;
 
 public interface TransactionService {
     void createTransaction(TransactionCreateDto transactionDto);
+    List<Transaction> getTransactionHistory(Long accountId);
+    List<Transaction> getAllTransactions();
+    List<Transaction> getPendingTransactions();
+    void approveTransaction(Long transactionId);
+    void rollbackTransaction(Long transactionId);
+    void deleteTransaction(Long id);
 }
