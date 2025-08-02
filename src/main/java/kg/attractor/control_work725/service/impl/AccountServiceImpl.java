c403774edd1fa66dbe82e3d68ded7556c72ae000
nargiza-jd <supabaevanargiza@gmail.com>
2025-08-02 12:37:14 +0600

    Created the AccountServiceImpl class

---
+++ src/main/java/kg/attractor/control_work725/service/impl/AccountServiceImpl.java
@@ -0,0 +1,27 @@
+package kg.attractor.control_work725.service.impl;
+
+import kg.attractor.control_work725.dao.AccountDao;
+import kg.attractor.control_work725.dao.UserDao;
+import kg.attractor.control_work725.model.Account;
+import kg.attractor.control_work725.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+    private final AccountDao accountDao;
+    private final UserDao userDao;
+
+    @Override
+    public void createAccount(AccountCreateDto accountDto) {
+        Long currentUserId = 1L;
+
+        Account account = new Account();
+        account.setUserId(currentUserId);
+        account.setCurrency(accountDto.getCurrency());
+        account.setBalance(0.0);
+
+        accountDao.save(account);
+    }
+}
