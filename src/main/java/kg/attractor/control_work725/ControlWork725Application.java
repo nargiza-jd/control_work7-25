b7d1a363c1977e90d96340597e9a7e5c4336e9c3
nargiza-jd <supabaevanargiza@gmail.com>
2025-08-02 11:40:40 +0600

    Added the necessary dependencies to the pom file and renamed the application file to yml

--- src/main/java/kg/attractor/control_work725/ControlWork725Application.java
+++ src/main/java/kg/attractor/control_work725/ControlWork725Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ControlWork725Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControlWork725Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ControlWork725Application.class, args);
+    }
 
 }
