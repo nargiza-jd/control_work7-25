48ca5b11e1335a36fcb61be7733bc1e984544ef8
nargiza-jd <supabaevanargiza@gmail.com>
2025-08-02 10:14:20 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/control_work725/ControlWork725Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.control_work725;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlWork725Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlWork725Application.class, args);
+	}
+
+}
---
+++ src/test/java/kg/attractor/control_work725/ControlWork725ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.control_work725;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlWork725ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
